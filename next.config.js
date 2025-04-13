34cd291aec5e8fceea08f12d40e1e1b038f4ee53
Anonymous096 <sakshamsharmaofficial7@gmail.com>
2025-04-14 04:27:03 +0530

    PATCH: backend deployed on GCP

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "deepimg-794052010786.europe-west4.run.app"],
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
-        port: "5001",
+        port: "8080",
         pathname: "/api/**",
       },
     ],
